04d26c22759f755d4f64646413e78b640075ea5d
张衡 <zhangheng1@forensix.cn>
2023-08-25 16:03:45 +0800

    init: 初步完成了响应式 React

---
+++ VueComponent.tsx
@@ -0,0 +1,56 @@
+
+import React from "react";
+
+class VueComponent extends React.Component {
+  template: any = undefined
+  state = {
+    count: 1
+  }
+  constructor(props) {
+    super(props)
+    this.setup()
+  }
+  // 副作用依赖收集
+  effectMap = new Map<any, any[]>()
+  _currentEffectCallback = null
+  watchEffect(cb) {
+    this._currentEffectCallback = cb
+    cb()
+    this._currentEffectCallback = null
+  }
+  update() {
+    this.setState({ count: this.state.count + 1 })
+  }
+  ref(initValue) {
+    const that = this
+    let refObj = {
+      value: initValue,
+      // 使用了这个状态的副作用
+      refCallbcak: [] as any[],
+    }
+    return new Proxy(refObj, {
+      get(target, key) {
+        // 是否是在副作用依赖收集期间触发，如果是就开始收集依赖
+        if(that._currentEffectCallback) {
+          refObj.refCallbcak.push(that._currentEffectCallback)
+        }
+        return target[key]
+      },
+      set(target, key, value) {
+        target[key] = value
+        that.update()
+        // 当前值变化，触发依赖回调
+        for(const callback of refObj.refCallbcak) {
+          callback()
+        }
+        return true
+      },
+    })
+  }
+  render() {
+    return this.template()
+  }
+  setup() {}
+}
+
+export default VueComponent
---
+++ index.tsx
@@ -0,0 +1,35 @@
+import { Button } from "antd";
+import VueComponent from "./VueComponent";
+ 
+class Test extends VueComponent {
+  setup() {
+    // 可以通过 ref 声明状态
+    // setup 只在初始化时执行一次，所以通过 ref 声明状态时条件、循环也没有关系
+    const a = this.ref(0)
+    const b = this.ref(0)
+
+    // 某种依赖于值的副作用，不需要声明依赖项，自动依赖追踪
+    this.watchEffect(() => {
+      console.log("A 点击次数：", a.value)
+    })
+    this.watchEffect(() => {
+      console.log("B 点击次数：", b.value)
+    })
+    
+    // 编写视图，相当于以前的 render，不过可以直接用闭包引用状态，和 this 没有关系
+    this.template = () => {
+      return (
+        <div>
+          <Button onClick={() => a.value++}>
+            A 按钮 { a.value }
+          </Button>
+          <Button onClick={() => b.value++}>
+            B 按钮 { b.value }
+          </Button>
+        </div>
+      )
+    }
+  }
+}
+
+export default Test
